fef821d4903d98979cccc0285d20a81e1678c0fe
Muzaffar Ahmad <muzaffarnissar786@gmail.com>
2025-02-25 08:42:08 +0530

    added my new file

--- test_farhat.py
+++ test_farhat.py
@@ -1,6 +1,6 @@
 movies_database = [
-   { "movie_name" : "Tare zameen par", "show_time" : "10.55", "price_ticket" : 500, "available_seats": ["20","30"]},
-   {"movie_name": "Chicichore", "show_time": "09.00", "price_tic": 500, "availble_seats" : ["40", "32"]},
+   { "movie_name" : "Tare zameen par", "show_time" : ["10.55", "08.30"], "price_ticket" : "500", "available_seats": "20"},
+   {"movie_name": "Chicichore", "show_time": ["01.55", "02.30"], "price_tic": "500", "availble_seats" :  "80"},
 ]
 
 
@@ -8,6 +8,10 @@ def displaymovies():
     print("\n All Movies")
     for i in movies_database:
         for key, value in i.items():
-            print(f"{key}, {value} \n")
+            print(f",{key}, {value} \n")
+
+        print(".............")
+
+
 
 displaymovies()
