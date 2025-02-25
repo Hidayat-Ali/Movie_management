6b0379b77270ff74bb6a8062f0dff0114171cfe0
Muzaffar Ahmad <muzaffarnissar786@gmail.com>
2025-02-25 09:06:48 +0530

    added my code

--- test.py
+++ test.py
@@ -9,21 +9,21 @@
 # Find the Movie in database 
 # Cancel the Booking 
 
-data = [
-  {"movie_name":"Avengers","show_time":["10","12"],"available_seats":["20","30"]},
-  {"movie_name":"krish","show_time":["10","12"],"available_seats":["20","30"]},
+# data = [
+#   {"movie_name":"Avengers","show_time":["10","12"],"available_seats":["20","30"]},
+#   {"movie_name":"krish","show_time":["10","12"],"available_seats":["20","30"]},
               
-]
+# ]
 
-bookings=[]
+# bookings=[]
 
-def displayMovies():
-    print(" \n All Movies ")
-    for i in data:
-        for key,value in i.items():
-            print(f"{key},{value} \n")
-        for time ,seat_avalaiblity in enumerate(i['show_time']):
-            print(time)
+# # def displayMovies():
+# #     print(" \n All Movies ")
+# #     for i in data:
+# #         for key,value in i.items():
+# #             print(f"{key},{value} \n")
+# #         for time ,seat_avalaiblity in enumerate(i['show_time']):
+# #             print(time)
 
         
 
@@ -33,7 +33,7 @@ def displayMovies():
     
 
 
-displayMovies()
+# displayMovies()
 
 
 
@@ -41,4 +41,20 @@ displayMovies()
 
 
 
+movies_database = [
+   { "movie_name" : "Tare zameen par", "show_time" : ["10.55", "08.30"], "price_ticket" : "500", "available_seats": "20"},
+   {"movie_name": "Chicichore", "show_time": ["01.55", "02.30"], "price_tic": "500", "availble_seats" :  "80"},
+]
 
+
+def displaymovies():
+    print("\n All Movies")
+    for i in movies_database:
+        for key, value in i.items():
+            print(f",{key}, {value} \n")
+
+        print(".............")
+
+
+
+displaymovies()
