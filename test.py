1234ae29bf3e42226d4202fd9aa613266c99a15b
Muzaffar Ahmad <muzaffarnissar786@gmail.com>
2025-03-02 13:45:50 +0530

    some changes

--- test.py
+++ test.py
@@ -41,53 +41,103 @@
 
 #my code
 
-movies_database = [
-   { "movie_name" : "Tare zameen par", "show_time" : ["10.55", "08.30"], "price_ticket" : "500", "available_seats": 20},
-   {"movie_name": "Chicichore", "show_time": ["01.55", "02.30"], "price_tic": "500", "availble_seats" :  80},
-]
+# movies_database = [
+#    { "movie_name" : "Tare zameen par", "show_time" : ["10.55", "08.30"], "price_ticket" : "500", "available_seats": 20},
+#    {"movie_name": "Chicichore", "show_time": ["01.55", "02.30"], "price_tic": "500", "availble_seats" :  80},
+# ]
 
 
 
 
-def displaymovies():
-    print("\n All Movies")
-    for i in movies_database:
+# # def displaymovies():
+# #     print("\n All Movies")
+# #     for i in movies_database:
 
-        for key, value in i.items():
-           print(f"{key}, {value} \n")
+# #         for key, value in i.items():
+# #            print(f"{key}, {value} \n")
 
-        for time,seat in enumerate(i["show_time"]):
-            print(time)
+# #         for time,seat in enumerate(i["show_time"]):
+# #             print(time)
 
-        print(".............")
-
-
-
-displaymovies()
+# #         print(".............")
 
 
 
-
-#search movie
-
-def search_movie(search):
-    allmovies =[]
-    for i in range(len(movies_database)):
-        a = movies_database[i]["movie_name"]
-        allmovies.append(a)
-
-    for movie in allmovies:
-        search = input("Enter the moveie you want to search: ")
-        if search == movie:
-            print("The movies is there")
-        else:
-            print("Movie does not exist")
-
-
-search_movie(Patrner)
+# # displaymovies()
 
 
 
+
+# #search movie
+
+# def search_movie():
+#     allmovies =[]
+#     for i in range(len(movies_database)):
+#         a = movies_database[i]["movie_name"]
+#         allmovies.append(a)
+
+#     for movie in allmovies:
+#         search = input("Enter the moveie you want to search: ")
+#         if search == movie:
+#             print("The movies is there")
+#             break
+#         else:
+#             print("Movie does not exist")
+#             break
+
+
+# search_movie()
 
 
 #book movie
+
+
+
+movies_database = [
+    {"movie_name" : "Tare Zameen par", "price_ticket" : 500  , "show_time" : ["10.30", "12.30"], "available_tickets" : [10,20]},
+    {"movie_name" : "Krish", "price_ticket" : 900, "show_time" : ["10.56", "08.30"], "available_tickets" : [10,40]} 
+    ]
+
+
+
+#DISPLAY ALL MOVIES
+def displaymovies():
+    print("\n All Movies")
+    for movie in movies_database:
+        print("Movies_Name:", movie["movie_name"])
+        print("Ticket_Movie:", movie["price_ticket"])
+        
+        for index, time in enumerate(movie["show_time"]):
+            print("Timing:", time)
+            print("Avaible Seats", movie["available_tickets"][index])
+
+        print("................")
+
+
+#displaymovies()
+
+
+
+#BOOK MOVIE
+def book_movie():
+
+    for movie in movies_database:
+        input_movie = input("Enter the movie you want to watch: ")
+
+
+        if input_movie == movie["movie_name"]:
+            print("Yes Movie is here"
+            print("Book the ticket")
+            break
+        else:
+            print("No Movie is not here")
+
+
+book_movie()
+        
+
+        
+
+
+
+
